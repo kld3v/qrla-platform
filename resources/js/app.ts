c6b92efa7e154fb41de0d3cdb6738d01d2393c90
niko <nikogham@pm.me>
2024-09-11 20:37:39 +0100

    edit app try  to get pinia installed. enable access to views from inertia.

--- resources/js/app.ts
+++ resources/js/app.ts
@@ -13,15 +13,17 @@ import VueApexCharts from 'vue3-apexcharts'
 import VueTablerIcons from 'vue-tabler-icons'
 import VueScrollTo from 'vue-scrollto'
 import VueEasyLightbox from 'vue-easy-lightbox'
+import { createPinia } from 'pinia'
+const pinia = createPinia()
 
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel'
 
 createInertiaApp({
 	title: (title) => `${title} - ${appName}`,
-	resolve: (name) => resolvePageComponent(`./Pages/${name}.vue`, import.meta.glob<DefineComponent>('./Pages/**/*.vue')),
 	setup({ el, App, props, plugin }) {
 		createApp({ render: () => h(App, props) })
 			.use(plugin)
+			.use(pinia)
 			.use(ZiggyVue)
 			.use(vuetify)
 			.use(PerfectScrollbar)
@@ -34,6 +36,7 @@ createInertiaApp({
 			})
 			.mount(el)
 	},
+	resolve: (name) => resolvePageComponent(`./Pages/${name}.vue`, import.meta.glob<DefineComponent>('./Pages/**/*.vue')),
 	progress: {
 		color: '#4B5563',
 	},
