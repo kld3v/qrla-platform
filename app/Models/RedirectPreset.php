3eed365cf6fcef2f15c8d08c4a52f02a64781e58
ngham <63638951+kld3v@users.noreply.github.com>
2024-09-27 10:36:59 +0100

    Update RedirectPreset.php
    remove unnecessary boilerplate comments

--- app/Models/RedirectPreset.php
+++ app/Models/RedirectPreset.php
@@ -8,12 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class RedirectPreset extends Model
 {
     use HasFactory;
-
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array
-     */
+    
     protected $fillable = [
         'name',
         'thumbnail',
