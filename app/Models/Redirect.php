5444701edbf9ecb18736997c3872b69ca4372a42
ngham <63638951+kld3v@users.noreply.github.com>
2024-09-27 10:34:32 +0100

    Update Redirect.php
    remove boilerplate comments

--- app/Models/Redirect.php
+++ app/Models/Redirect.php
@@ -8,12 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class Redirect extends Model
 {
     use HasFactory;
-
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array
-     */
+    
     protected $fillable = [
         'base_url_id',
         'logo_id',
