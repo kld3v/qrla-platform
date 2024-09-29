5d78a35a51f4f8199606f36d223c4d14acf7d6b9
niko <nikogham@pm.me>
2024-09-29 19:57:36 +0100

    edit colors

--- resources/js/layouts/full/FullLayout.vue
+++ resources/js/layouts/full/FullLayout.vue
@@ -1,11 +1,7 @@
 <script setup lang="ts">
 import VerticalSidebarVue from './vertical-sidebar/VerticalSidebar.vue'
 import VerticalHeaderVue from './vertical-header/VerticalHeader.vue'
-import HorizontalHeader from './horizontal-header/HorizontalHeader.vue'
-import HorizontalSidebar from './horizontal-sidebar/HorizontalSidebar.vue'
-import Customizer from './customizer/Customizer.vue'
 import { useCustomizerStore } from '../../stores/customizer'
-import { pl, zhHans } from 'vuetify/locale'
 
 const customizer = useCustomizerStore()
 </script>
@@ -33,7 +29,6 @@ const customizer = useCustomizerStore()
 			</v-navigation-drawer>
 			<VerticalSidebarVue v-if="!customizer.setHorizontalLayout" />
 			<div :class="customizer.boxed ? 'maxWidth' : 'full-header'"><VerticalHeaderVue v-if="!customizer.setHorizontalLayout" /></div>
-			<div :class="customizer.boxed ? 'maxWidth' : 'full-header'"><HorizontalHeader v-if="customizer.setHorizontalLayout" /></div>
 
 			<v-main class="mr-md-4">
 				<div class="mb-3 hr-layout bg-containerBg">
