685f7a827c635e81a188aaea02414490cc6a02ea
niko <nikogham@pm.me>
2024-10-17 08:42:38 +0100

    get data in for all time scales

--- resources/js/Pages/BlockStats/Partials/PlaqueTimeGraph.vue
+++ resources/js/Pages/BlockStats/Partials/PlaqueTimeGraph.vue
@@ -33,8 +33,16 @@ watch(
 	() => props.selectedItem,
 	async (newVal, oldVal) => {
 		if (newVal && newVal.id !== oldVal?.id) {
-			let res = await getAccessesOverTime(props.idType, props.selectedItem.id, '1d')
-			console.log(res)
+			let res_day = await getAccessesOverTime(props.idType, props.selectedItem.id, '1d')
+			console.log('last day:', res_day)
+			let res_week = await getAccessesOverTime(props.idType, props.selectedItem.id, '1w')
+			console.log('last week:', res_week)
+			let res_month = await getAccessesOverTime(props.idType, props.selectedItem.id, '1m')
+			console.log('last month:', res_month)
+			let res_3m = await getAccessesOverTime(props.idType, props.selectedItem.id, '3m')
+			console.log('last 3m:', res_3m)
+			let res_1y = await getAccessesOverTime(props.idType, props.selectedItem.id, '1y')
+			console.log('last year: ', res_1y)
 		}
 	},
 	{ immediate: true } // Add immediate option if you want to call it on component mount as well
