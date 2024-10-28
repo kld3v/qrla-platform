1834bf88c43e58c08fe7a6a117e64e551c017c00
joelaharris98 <joelaharris98@gmail.com>
2024-10-28 16:40:49 +0000

    Removed Logs. Added Assets

--- app/Legacy/Services/ShortUrlShowService.php
+++ app/Legacy/Services/ShortUrlShowService.php
@@ -6,44 +6,28 @@ use App\Legacy\Models\ShortUrl;
 use Illuminate\Http\Request;
 use App\Legacy\Models\ContactCard;
 use Illuminate\Support\Facades\View;
-use Illuminate\Support\Facades\Log;
 use JeroenDesloovere\VCard\VCard;
 
 class ShortUrlShowService {
 
     public function show(Request $request, $short_code)
     {
-        Log::info("ShortUrlShowService::show - Started", ['short_code' => $short_code]);
-
         $shortUrl = ShortUrl::byShortCode($short_code)->firstOrFail();
-        Log::info("ShortUrlShowService::show - ShortUrl found", ['short_url_id' => $shortUrl->id]);
 
         if ($shortUrl->contactCard) {
-            Log::info("ShortUrlShowService::show - Contact card found, destination URL set", ['destination_url' => $shortUrl->destination_url]);
             return $this->contactCardShow($short_code);
         }
 
         if ($shortUrl->status === 'rejected') {
-            Log::info("ShortUrlShowService::show - Status is rejected, returning view.");
             return view('link_terminated');
         } elseif ($shortUrl->status === 'pending') {
             $shortUrl->destination_url = 'https://qrla.io/page/pending';
-            Log::info("ShortUrlShowService::show - Status is pending, destination URL set to pending.");
         }
 
         if ($shortUrl->redirect && $shortUrl->redirect->redirectPreset) {
             $htmlPath = $shortUrl->redirect->redirectPreset->html_path;
-            Log::info("ShortUrlShowService::show - Redirect preset found", ['html_path' => $htmlPath]);
             $filePath = base_path('app/Legacy/' . $htmlPath);
-            Log::info("ShortUrlShowService::show - Full file path for view", ['file_path' => $filePath]);
-
             $logoPath = $shortUrl->redirect->logo->path;
-
-            Log::info("ShortUrlShowService::show - Rendering view", [
-                'short_url' => $shortUrl->id,
-                'file_path' => $filePath,
-                'logo_path' => $logoPath,
-            ]);
 
             return View::file($filePath, [
                 'shortUrl' => $shortUrl,
@@ -51,27 +35,20 @@ class ShortUrlShowService {
                 'domain' => parse_url($shortUrl->destination_url, PHP_URL_HOST),
             ]);
         }
-
-        Log::info("ShortUrlShowService::show - No redirect preset found.");
     }
 
     public function contactCardShow($short_code)
     {
-        Log::info("ShortUrlShowService::contactCardShow - Started", ['short_code' => $short_code]);
-    
         $contactCard = ContactCard::whereHas('shortUrl', function ($query) use ($short_code) {
             $query->where('short_code', $short_code);
         })->firstOrFail();
-    
-        Log::info("ShortUrlShowService::contactCardShow - ContactCard found", ['contact_card_id' => $contactCard->id]);
-    
-        // Generate the vCard as a Base64 string
+
         $vcard = new VCard();
         $nameParts = explode(' ', $contactCard->name);
         $lastName = array_pop($nameParts);
         $firstName = implode(' ', $nameParts);
         $vcard->addName($lastName, $firstName);
-        
+
         if ($contactCard->company) {
             $vcard->addCompany($contactCard->company);
         }
@@ -88,24 +65,21 @@ class ShortUrlShowService {
             foreach ($contactCard->phone_numbers as $phone) {
                 $type = strtoupper($phone['type']);
                 $number = $phone['number'];
-    
+
                 if (!str_starts_with($number, '+44')) {
                     if (str_starts_with($number, '0')) {
                         $number = substr($number, 1);
                     }
                     $number = '+44' . $number;
                 }
-    
+
                 $vcard->addPhoneNumber($number, $type);
             }
         }
-    
+
         $vcardContent = base64_encode($vcard->getOutput());
-    
         $filePath = base_path('app/Legacy/views/contact_cards/show.blade.php');
-        Log::info("ShortUrlShowService::contactCardShow - Rendering view", ['file_path' => $filePath]);
-    
+
         return View::file($filePath, compact('contactCard', 'short_code', 'vcardContent'));
     }
-    
 }
