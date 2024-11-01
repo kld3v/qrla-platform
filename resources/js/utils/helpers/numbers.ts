9d43cc8d14db6db9d3c3d1f52c9719eb10b63283
niko <nikogham@pm.me>
2024-11-01 12:04:34 +0000

    fix number comma entry function to handle strings

--- resources/js/utils/helpers/numbers.ts
+++ resources/js/utils/helpers/numbers.ts
@@ -1,3 +1,4 @@
-export function formatNumberWithCommas(number: number) {
-	return number.toLocaleString()
+export function formatNumberWithCommas(number: number | string): string | number {
+	const parsedNumber = typeof number === 'string' ? parseFloat(number) : number
+	return isNaN(parsedNumber) ? number : parsedNumber.toLocaleString()
 }
