dacab491973454a809048c3f07325b5801229589
joelaharris98 <joelaharris98@gmail.com>
2024-11-15 12:52:28 +0000

    Update 01_apply_nginx_config.sh

--- .platform/hooks/predeploy/01_apply_nginx_config.sh
+++ .platform/hooks/predeploy/01_apply_nginx_config.sh
@@ -4,14 +4,14 @@
 set -e
 
 # Log the environment variable for debugging
-echo "Deploying to ENVIRONMENT: $ENVIRONMENT"
+echo "Deploying to ENVIRONMENT: $APP_ENV"
 
 # Define the source and destination paths
 NGINX_CONF_DIR="/etc/nginx/conf.d"
 PLATFORM_NGINX_CONF_DIR="/var/app/staging/.platform/nginx"
 
 # Select the appropriate Nginx configuration
-if [ "$ENVIRONMENT" == "production" ]; then
+if [ "$APP_ENV" == "production" ]; then
     cp "$PLATFORM_NGINX_CONF_DIR/production.conf" "$NGINX_CONF_DIR/custom.conf"
     echo "Applied production Nginx configuration."
 else
